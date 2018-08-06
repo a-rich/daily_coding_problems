02eef8078a70b48bebb1d8f455dec7729171b2b7
Alex Richards <alex.richards006@gmail.com>
2018-08-06 15:10:40 -0700

    fixed first missing positive number solution

--- first_missing_positive_number.py
+++ first_missing_positive_number.py
@@ -1,20 +1,22 @@
-from __future__ import print_function
-
 def find_missing(arr):
     arr.sort()
     i = 0
 
     while i < len(arr):
         if arr[i] < 1:
+            i += 1
             continue
-        if arr[i] != arr[i-1] + 1:
+        if arr[i-1] > 0 and arr[i] != arr[i-1] + 1:
             return arr[i-1] + 1
+        i += 1
 
     return arr[-1] + 1
 
-if __name__ == '___main__':
+if __name__ == '__main__':
     test_1 = [3,4,-1,1]
     test_2 = [1,2,0]
 
     assert find_missing(test_1) == 2
     assert find_missing(test_2) == 3
+
+
