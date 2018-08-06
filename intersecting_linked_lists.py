b2eaf448365a89e50d25b48d231bd7caa7cc9d51
Alex Richards <alex.richards006@gmail.com>
2018-08-06 10:56:22 -0700

    intersecting linked lists solution complete

---
+++ intersecting_linked_lists.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.next = None
+
+    def get_value(self):
+        return self.value
+
+    def set_next(self, node):
+        self.next = node
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return "Node({})".format(self.value)
+
+class LinkedList:
+    def __init__(self, nodes=[]):
+        self.head = None
+
+        if nodes:
+            self.head = Node(nodes.pop(0))
+            last = self.head
+            while nodes:
+                temp = Node(nodes.pop(0))
+                last.set_next(temp)
+                last = temp
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.next
+
+    def __str__(self):
+        return "[{}]".format(", ".join(map(str, self)))
+
+    def __repr__(self):
+        return "LinkedList({})".format(self)
+
+def find_intersection(ll_a, ll_b):
+    cache = {}
+
+    for node in ll_a:
+        cache[node.get_value()] = 1
+
+    for node in ll_b:
+        if node.get_value() in cache:
+            return node
+
+    return "These linked lists do not intersect."
+
+if __name__ == '__main__':
+    # Tests two LinkedLists that intersect at their final nodes.
+    ll_a = LinkedList([1,2,3])
+    ll_b = LinkedList([5,4,3])
+    print(find_intersection(ll_a, ll_b))
+
+    # Tests two LinkedLists that do not intersect at all.
+    ll_a = LinkedList([1,2,3])
+    ll_b = LinkedList([6,5,4])
+    print(find_intersection(ll_a, ll_b))
+
+    # Tests two linked lists that intersect somewhere in the middle.
+    ll_a = LinkedList([3,7,8,10])
+    ll_b = LinkedList([99, 1, 8, 10])
+    print(find_intersection(ll_a, ll_b))
