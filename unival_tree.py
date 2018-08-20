0454339f932f346a0eb7c59cd6499df40c1c45e4
Alex Richards <alex.richards006@gmail.com>
2018-08-20 00:18:13 -0700

    unival tree solution complete

---
+++ unival_tree.py
@@ -0,0 +1,41 @@
+from BinaryTree import BinaryTree, Node
+
+def num_unival_trees(node):
+    count = [0]
+    num_unival_subtrees(node, count)
+
+    return count[0]
+
+def num_unival_subtrees(node, count=0):
+    if node is None:
+        return True
+
+    left = num_unival_subtrees(node.left, count)
+    right = num_unival_subtrees(node.right, count)
+
+    if left == False or right == False:
+        return False
+
+    if node.left and node.data != node.left.data:
+        return False
+
+    if node.right and node.data != node.right.data:
+        return False
+
+    count[0] += 1
+
+    return True
+
+
+
+if __name__ == '__main__':
+    bt = BinaryTree(0)
+    bt.root.left = Node(1)
+    bt.root.right = Node(0)
+    bt.root.right.left = Node(1)
+    bt.root.right.right = Node(0)
+    bt.root.right.left.left = Node(1)
+    bt.root.right.left.right = Node(1)
+
+    assert num_unival_trees(bt.root) == 5
+
