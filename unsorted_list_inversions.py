7d79a66116cacf81585b9c6548fe2d0555a0cc64
Alex Richards <alex.richards006@gmail.com>
2018-08-30 13:05:12 -0700

    replaced nonoptimal heap based solution with mergesort solution

--- unsorted_list_inversions.py
+++ unsorted_list_inversions.py
@@ -1,9 +1,5 @@
-from __future__ import print_function
-import heapq
-
 def naive_count_inversions(arr):
     i = count = 0
-
     while i < len(arr):
         j = i + 1
         while j < len(arr):
@@ -15,23 +11,32 @@ def naive_count_inversions(arr):
     return count
 
 def opt_count_inversions(arr):
-    inversions = 0
-    heap = list(arr)
-    heapq.heapify(heap)
+    if len(arr) == 1:
+        return arr, 0
 
-    while heap:
-        for n in arr:
-            if n > heap[0]:
-                inversions += 1
-            elif n == heap[0]:
-                heapq.heappop(heap)
-                break
+    left, li = opt_count_inversions(arr[:len(arr)/2])
+    right, ri = opt_count_inversions(arr[len(arr)/2:])
+    merged = []
 
-    return inversions
+    i = j = 0
+    inversions = li + ri
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+            inversions += len(left) - i
+
+    merged += left[i:]
+    merged += right[j:]
+
+    return merged, inversions
 
 if __name__ == '__main__':
     arr = [2,4,1,3,5]
-    assert opt_count_inversions(arr) == 3
+    assert opt_count_inversions(arr)[1] == 3
 
     arr = [5,4,3,2,1]
-    assert opt_count_inversions(arr) == 10
+    assert opt_count_inversions(arr)[1] == 10
