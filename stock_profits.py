6400b188a488f5320b1cdc0b3c6dd863d6c1bee9
Alex Richards <alex.richards006@gmail.com>
2018-09-14 10:35:47 -0700

    stock profit solution complete

---
+++ stock_profits.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+def naive_profit(prices):
+    max_profit = float('-inf')
+    for i, price in enumerate(prices):
+        if i == 0:
+            continue
+
+        for j, p in enumerate(prices[:i]):
+            if price - p > max_profit:
+                max_profit = price - p
+
+    return max_profit
+
+def opt_profit(prices):
+    min_val = float('inf')
+    max_profit = float('-inf')
+    for price in prices:
+        if price < min_val:
+            min_val = price
+        if price - min_val > max_profit:
+            max_profit = price - min_val
+
+    return max_profit
+
+if __name__ == '__main__':
+    prices = [9,11,8,5,7,10]
+    assert opt_profit(prices) == 5
